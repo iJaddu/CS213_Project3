5a547dedc220015f6e5ca52e50401e3a789af694
NightOnTheSun <nightonthesun@protonmail.com>
2020-10-27 22:44:17 -0400

    Started some action methods in Controller, Added import/export buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.awt.color.ProfileDataException;
+import java.util.StringTokenizer;
 
 public class Controller {
 
@@ -49,30 +53,92 @@ public class Controller {
 
     @FXML
     private Button withdrawButton;
+    @FXML
+    private Button importButton, exportButton;
+    @FXML
+    private TextArea outputArea;
+
+    private TextField dateOpenedDeposit;
 
 
     @FXML
     void clearTextFields(ActionEvent event) {
 
+        firstNameOpen.clear();
+        firstNameClose.clear();
+        firstNameDeposit.clear();
+        firstNameWithdraw.clear();
+        lastNameOpen.clear();
+        lastNameClose.clear();
+        lastNameDeposit.clear();
+        lastNameWithdraw.clear();
+        monthOpen.clear();
+        monthClose.clear();
+        balanceOpen.clear();
+        balanceClose.clear();
+        dayOpen.clear();
+        dayClose.clear();
+        yearOpen.clear();
+        yearClose.clear();
+        amountDeposit.clear();
+        amountWithdraw.clear();
+        dateOpenedDeposit.clear();
+        outputArea.clear();
     }
 
     @FXML
     void closeAccount(ActionEvent event) {
 
+        Profile profile = new Profile();
+        profile.setFname(firstNameClose.getText());
+        profile.setLname(lastNameClose.getText());
+        
     }
 
     @FXML
     void openAccount(ActionEvent event) {
+
+        Profile profile = new Profile();
+        profile.setFname(firstNameOpen.getText());
+        profile.setLname(lastNameOpen.getText());
 
     }
 
     @FXML
     void deposit(ActionEvent event) {
 
+        Profile profile = new Profile();
+        profile.setFname(firstNameDeposit.getText());
+        profile.setLname(lastNameDeposit.getText());
+
+        Date date = new Date();
+        StringTokenizer st = new StringTokenizer(dateOpenedDeposit.getText(), "/");
+
+        while(st.hasMoreElements()) {
+            date.setMonth(st.nextElement());
+            date.setDay(st.nextElement());
+            date.setYear(st.nextElement());
+        }
+
+        double depositAmount = Double.parseDouble(amountDeposit.getText()); // Temp
+
+
     }
 
     @FXML
     void withdraw(ActionEvent event) {
+
+        Profile profile = new Profile();
+        profile.setFname(firstNameWithdraw.getText());
+        profile.setLname(lastNameWithdraw.getText());
+
+    }
+    @FXML
+    void importFile(ActionEvent event) {
+
+    }
+    @FXML
+    void exportFile(ActionEvent event) {
 
     }
 
