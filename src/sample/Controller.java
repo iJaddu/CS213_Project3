6ce99a73553ef737d6caecc71cc05810e38849bf
iJaddu <Computers99!!>
2020-10-28 19:59:39 -0400

    fixed bunch of GUI stuff and buttons and how they work/interact.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,67 +2,33 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.util.StringTokenizer;
 
 public class Controller {
 
     @FXML
-    private TextField firstNameOpen, firstNameClose, firstNameDeposit, firstNameWithdraw;
+    private TextField firstNameOpen, lastNameOpen, balanceOpen, firstNameClose, lastNameClose, balanceClose, firstNameDeposit, firstNameWithdraw, lastNameDeposit, lastNameWithdraw, amountDeposit, amountWithdraw, monthOpen, dayOpen, yearOpen, monthClose, dayClose, yearClose;
 
     @FXML
-    private TextField lastNameOpen, lastNameClose, lastNameDeposit, lastNameWithdraw;
+    private Button openAccountButton, closeAccountButton, clearButtonOpen, accountType, clearButtonClose, depositButton, withdrawButton, importButton, exportButton;
 
     @FXML
-    private TextField monthOpen, monthClose;
+    private RadioButton CheckingRadioButton, SavingsRadioButton, MMRadioButton;
 
     @FXML
-    private TextField balanceOpen, balanceClose;
+    private CheckBox directDepositOpen, loyalCustomerOpen, directDepositClose, loyalCustomerClose;
 
-    @FXML
-    private TextField dayOpen, dayClose;
-
-    @FXML
-    private TextField yearOpen, yearClose;
-
-    @FXML
-    private Button openAccountButton;
-
-    @FXML
-    private Button clearButtonOpen;
-
-    @FXML
-    private RadioButton accountType;
-
-    @FXML
-    private Button closeAccountButton;
-
-    @FXML
-    private Button clearButtonClose;
-
-    @FXML
-    private TextField amountDeposit, amountWithdraw;
-
-    @FXML
-    private Button depositButton;
-
-    @FXML
-    private Button withdrawButton;
-    @FXML
-    private Button importButton, exportButton;
     @FXML
     private TextArea outputArea;
 
-    private TextField dateOpenedDeposit;
+    @FXML
+    private ToggleGroup acctype;
 
 
     @FXML
     void clearTextFields(ActionEvent event) {
-
         firstNameOpen.clear();
         firstNameClose.clear();
         firstNameDeposit.clear();
@@ -71,76 +37,149 @@ public class Controller {
         lastNameClose.clear();
         lastNameDeposit.clear();
         lastNameWithdraw.clear();
-        monthOpen.clear();
         monthClose.clear();
-        balanceOpen.clear();
-        balanceClose.clear();
+        monthOpen.clear();
         dayOpen.clear();
         dayClose.clear();
-        yearOpen.clear();
         yearClose.clear();
+        yearOpen.clear();
+        balanceOpen.clear();
+        balanceClose.clear();
         amountDeposit.clear();
         amountWithdraw.clear();
-        dateOpenedDeposit.clear();
         outputArea.clear();
     }
+
+
+    @FXML
+    void mmButtonOnClickedOpen(ActionEvent event){
+        loyalCustomerOpen.setDisable(true);
+        directDepositOpen.setDisable(true);
+    }
+
+    @FXML
+    void savingsButtonOnClickedOpen(ActionEvent event){
+        directDepositOpen.setDisable(true);
+        loyalCustomerOpen.setDisable(false);
+    }
+
+    @FXML
+    void checkingButtonOnClickedOpen(ActionEvent event){
+        loyalCustomerOpen.setDisable(true);
+        directDepositOpen.setDisable(false);
+    }
+
+    @FXML
+    void mmButtonOnClickedClose(ActionEvent event){
+        loyalCustomerClose.setDisable(true);
+        directDepositClose.setDisable(true);
+    }
+
+    @FXML
+    void savingsButtonOnClickedClose(ActionEvent event){
+        directDepositClose.setDisable(true);
+        loyalCustomerClose.setDisable(false);
+    }
+
+    @FXML
+    void checkingButtonOnClickedClose(ActionEvent event){
+        loyalCustomerClose.setDisable(true);
+        directDepositClose.setDisable(false);
+    }
+
+
+
 
     @FXML
     void closeAccount(ActionEvent event) {
 
-        //Profile profile = new Profile();
-        //profile.setFname(firstNameClose.getText());
-        //profile.setLname(lastNameClose.getText());
-        
+        try{
+            if(MMRadioButton.isSelected()){
+
+            }else if(SavingsRadioButton.isSelected()){
+
+            }else if(CheckingRadioButton.isSelected()){
+
+            }
+
+            Profile profile = new Profile();
+            profile.setFname(firstNameClose.getText());
+            profile.setLname(lastNameClose.getText());
+
+        }catch (Exception e){
+
+        }
+
+
     }
 
     @FXML
     void openAccount(ActionEvent event) {
+        try{
+            if(MMRadioButton.isSelected()){
 
-        //Profile profile = new Profile();
-        //profile.setFname(firstNameOpen.getText());
-        //profile.setLname(lastNameOpen.getText());
+            }else if(SavingsRadioButton.isSelected()){
+
+            }else if(CheckingRadioButton.isSelected()){
+
+            }
+
+            Profile profile = new Profile();
+            profile.setFname(firstNameClose.getText());
+            profile.setLname(lastNameClose.getText());
+
+            int year = Integer.parseInt(yearOpen.getText());
+            int month = Integer.parseInt(monthOpen.getText());
+            int day = Integer.parseInt(dayOpen.getText());
+            Date date = new Date(year, month, day);
+
+
+
+
+            double depositAmount = Double.parseDouble(amountDeposit.getText()); // Temp
+
+
+
+        }catch (Exception e){
+
+        }
 
     }
 
     @FXML
     void deposit(ActionEvent event) {
+        try{
+            Profile profile = new Profile();
+            profile.setFname(firstNameDeposit.getText());
+            profile.setLname(lastNameDeposit.getText());
 
-        /*
-        Profile profile = new Profile();
-        profile.setFname(firstNameDeposit.getText());
-        profile.setLname(lastNameDeposit.getText());
-
-        Date date = new Date(0, 0, 0);
-        StringTokenizer st = new StringTokenizer(dateOpenedDeposit.getText(), "/");
-
-        while(st.hasMoreElements()) {
-            date.setMonth(st.nextElement());
-            date.setDay(st.nextElement());
-            date.setYear(st.nextElement());
+        }catch (Exception e){
 
         }
-
-        double depositAmount = Double.parseDouble(amountDeposit.getText()); // Temp
-        */
 
     }
 
     @FXML
     void withdraw(ActionEvent event) {
+        try{
+            Profile profile = new Profile();
+            profile.setFname(firstNameWithdraw.getText());
+            profile.setLname(lastNameWithdraw.getText());
+        }catch (Exception e){
 
-        //Profile profile = new Profile();
-        //profile.setFname(firstNameWithdraw.getText());
-        //profile.setLname(lastNameWithdraw.getText());
+        }
 
     }
-    @FXML
-    void importFile(ActionEvent event) {
 
-    }
     @FXML
     void exportFile(ActionEvent event) {
 
     }
+
+    @FXML
+    void importFile(ActionEvent event) {
+
+    }
+
 
 }
